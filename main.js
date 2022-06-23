3903944314e771d1efa77b2ffb6ca0db5dc867e0
kchaw1 <heatarin@gmail.com>
2022-06-23 18:11:18 +0900

    dev L4 end

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            inventory: 0
+            inStock: false,
+            inventory: 1,
+            details: ['50% cototn', '30% wool', '20% polyester'],
+            variants: [
+                {id: 1, color:'green'},
+                {id: 2, color:'blue'}
+            ]
         }
     }
 })
